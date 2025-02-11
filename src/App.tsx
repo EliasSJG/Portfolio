8ef5f65d1ae3457f480da02c8017af5114c810af
Elias Gustafsson <elias_gustafsson@outlook.com>
2025-02-11 09:22:45 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <h1>Portfolio</h1>
+
+      <p>Ett övningsprojekt, samt ett portfolio</p>
+    </>
+  );
+}
+
+export default App;

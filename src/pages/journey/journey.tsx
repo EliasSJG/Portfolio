47cd896fc5edd52fda6e680c521bf08b7f2cccc1
Elias Gustafsson <elias_gustafsson@outlook.com>
2025-02-20 11:20:44 +0100

    cleaned temp code, projects + layout

---
+++ src/pages/journey/journey.tsx
@@ -0,0 +1,8 @@
+import "./_journey.scss";
+export default function Journey() {
+  return (
+    <div>
+      <h1>Journey</h1>
+    </div>
+  );
+}
--- src/pages/start/start.tsx
+++ src/pages/start/start.tsx
@@ -1,6 +1,33 @@
 import "./_start.scss";
 import "../../styles/base/_typography.scss";
+import Button from "../../components/button/button";
+import { useRef } from "react";
+import Project from "../project/project";
 function Start() {
+  const sectionScroll = useRef<HTMLDivElement | null>(null);
+  const scrollToProjects = () => {
+    sectionScroll.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const projects = [
+    {
+      title: "Ultimate Trivia",
+      description:
+        "Detta är ett slutprojekt app jag skapade för den tredje kursen i skolan. Det är ett frågesportsspel som är en blandning mellan smartare än en femteklassare och postkodsmiljonären",
+      link: "https://github.com/EliasSJG/UltimateTrivia",
+    },
+    {
+      title: "Zoodjur",
+      description: "tomt just nu",
+      link: "",
+    },
+
+    {
+      title: "Monster Creation",
+      description: "tomt just nu",
+      link: "",
+    },
+  ];
   return (
     <>
       <div className="start">
@@ -11,7 +38,15 @@ function Start() {
             Just nu en övning för att lära om react, men kommer stadigt
             utvecklas till en riktig Portfolio som kan vara bra att ha
           </p>
+          <Button
+            handleClick={scrollToProjects}
+            title="Lär mer om mig!"
+          ></Button>
         </div>
+      </div>
+
+      <div ref={sectionScroll}>
+        <Project projects={projects} />
       </div>
     </>
   );

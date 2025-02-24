cfa5531880d3f17b8cef33d9bb463b8b73dceb42
Elias <you@example.com>
2025-02-24 10:37:10 +0100

    journey not interactive

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,38 @@
 import Header from "./components/header/header";
+import "./styles/base/_typography.scss";
+import { useRef } from "react";
+import { journey, projects } from "./state/state";
+import Button from "./components/button/button";
+import Project from "./pages/project/project";
+import "./pages/start/_start.scss";
 import Journey from "./pages/journey/journey";
-import Start from "./pages/start/start";
-
-function App() {
+export default function App() {
+  const sectionScroll = useRef<HTMLDivElement | null>(null);
+  const scrollToProjects = () => {
+    sectionScroll.current?.scrollIntoView({ behavior: "smooth" });
+  };
   return (
     <>
       {" "}
-      <Header></Header> <Start></Start>
-      <Journey></Journey>
+      <Header></Header>
+      <div className="start">
+        <div>
+          <h1>Elias Gustafsson</h1>
+          <h2>Portfolio</h2>
+          <p>
+            Just nu en övning för att lära om react, men kommer stadigt
+            utvecklas till en riktig Portfolio som kan vara bra att ha
+          </p>
+          <Button
+            handleClick={scrollToProjects}
+            title="Lär mer om mig!"
+          ></Button>
+        </div>
+      </div>
+      <div ref={sectionScroll}>
+        <Project projects={projects} />
+      </div>{" "}
+      <Journey journey={journey} />
     </>
   );
 }
-export default App;
--- src/pages/journey/journey.tsx
+++ src/pages/journey/journey.tsx
@@ -1,8 +1,26 @@
 import "./_journey.scss";
-export default function Journey() {
+
+type JourneyProps = {
+  journey: {
+    year: string;
+    description: string;
+  }[];
+};
+
+export default function Journey({ journey }: JourneyProps) {
   return (
-    <div>
-      <h1>Journey</h1>
-    </div>
+    <>
+      <h1 className="journey-heading">Min Resa</h1>
+      <div className="journey-div">
+        {journey.map((journey, index) => {
+          return (
+            <div key={index}>
+              <h2>{journey.year}</h2>
+              <p>{journey.description}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
